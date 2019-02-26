bf7f8c67143056dc86693f313ac826e00dd613c6
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-02-26 21:25:49 +0000

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,37 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/template.html",
+      filename: "./index.html",
+      favicon: "./docs/assets/favicon.ico"
+    })
+  ]
 };
