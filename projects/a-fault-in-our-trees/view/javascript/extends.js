d1c5420b1717f590468c35c8a0e6d5c4979e7b32
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-02-26 20:34:26 +0000

    Removed old project files

--- projects/a-fault-in-our-trees/view/javascript/extends.js
+++
@@ -1,5 +0,0 @@
-var __extends = this.__extends || function (child, parent) {
-	function Proxy() { this.constructor = child; }
-	Proxy.prototype = parent.prototype;
-	child.prototype = new Proxy();
-}
