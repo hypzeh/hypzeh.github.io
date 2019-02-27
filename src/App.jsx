7dfa6311b75b877d4fb8765203d29c9671b6ec9d
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-02-27 22:07:38 +0000

    Added a global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './styles/globalStyle';
+
 const App = () => (
-  <h1>Hello World!</h1>
+  <React.Fragment>
+    <GlobalStyle />
+    <h1>Hello World!</h1>
+  </React.Fragment>
 );
 
 export default App;
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    background: #212121;
+    color: #fff;
+    padding: 1em;
+    line-height: 1.8em;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeSpeed;
+    word-wrap: break-word;
+  }
+`;
+
+export default GlobalStyle;
