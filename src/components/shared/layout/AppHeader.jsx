9e3f724dca55f9ca331d02b6e747959ca9c6fc57
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-02-27 22:58:40 +0000

    Created AppHeader component

---
+++ src/components/shared/layout/AppHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AppHeader = () => (
+  <header>
+    <h1>Nick Smirnoff</h1>
+  </header>
+);
+
+export default AppHeader;
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
+  BrowserRouter, Switch, Route, Link,
 } from 'react-router-dom';
 
+import AppHeader from '../components/shared/layout/AppHeader';
 import AboutContainer from '../components/about/AboutContainer';
 
 const AppRouter = () => (
   <BrowserRouter>
-    <Switch>
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <React.Fragment>
-            <h1>Home.</h1>
-            <Link to="/about">About</Link>
-          </React.Fragment>
-        )}
-      />
-      <Route
-        exact
-        path="/about"
-        component={AboutContainer}
-      />
-    </Switch>
+    <React.Fragment>
+      <AppHeader />
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => (
+            <React.Fragment>
+              <h1>Home.</h1>
+              <Link to='/about'>About</Link>
+            </React.Fragment>
+          )}
+        />
+        <Route exact path='/about' component={AboutContainer} />
+      </Switch>
+    </React.Fragment>
   </BrowserRouter>
 );
 
