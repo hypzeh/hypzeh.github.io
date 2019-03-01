ddd27002d21c5bf9474bf1a32f7d06294ebd524e
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-01 21:45:56 +0000

    Added thumbnail property to projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -2,6 +2,7 @@ const projects = [
   {
     title: 'test project',
     tags: ['#programming', '#test'],
+    thumbnail: 'http://placekitten.com/300/200',
     description: 'this is a description about the project',
   },
 ];
