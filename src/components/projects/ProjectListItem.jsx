e4b4bbaa2761837a1784f8244fe44e6020655c82
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-01 23:08:59 +0000

    Added project item properties to view

--- src/components/projects/ProjectListItem.jsx
+++ src/components/projects/ProjectListItem.jsx
@@ -5,6 +5,9 @@ import ProjectListItemTypes from '../../types/projects/projectListItem';
 const ProjectListItem = ({ project }) => (
   <React.Fragment>
     <h1>{project.title}</h1>
+    <img src={project.thumbnail} />
+    <p>{project.description}</p>
+    <div>{project.tags.map(tag => ` ${tag}`)}</div>
   </React.Fragment>
 );
 
