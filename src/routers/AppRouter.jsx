ca2180493a41dd4f8ea2a35ea75280bc8253e488
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-01 19:38:33 +0000

    Created HomeContainer component

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import AppHeader from '../components/shared/layout/AppHeader';
+import HomeContainer from '../components/home/HomeContainer';
 import AboutContainer from '../components/about/AboutContainer';
 import ProjectsContainer from '../components/projects/ProjectsContainer';
 
@@ -10,7 +11,7 @@ const AppRouter = () => (
     <React.Fragment>
       <AppHeader />
       <Switch>
-        <Route exact path='/' render={() => <h1>Home.</h1>} />
+        <Route exact path='/' component={HomeContainer} />
         <Route exact path='/about' component={AboutContainer} />
         <Route exact path='/projects' component={ProjectsContainer} />
       </Switch>
