bdabf3e1eea5d44848c053fae91de2244dce2c15
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-01 19:49:06 +0000

    Created project lib

---
+++ src/lib/projects.js
@@ -0,0 +1,9 @@
+const projects = [
+  {
+    title: 'test project',
+    tags: ['#programming', '#test'],
+    description: 'this is a description about the project',
+  },
+];
+
+export default projects;
