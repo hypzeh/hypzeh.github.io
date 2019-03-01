eb1b9bfceab2fe39be8d93e0126bd146a524a530
Nick Smirnoff <nick.smirnoff@live.co.uk>
2019-03-01 22:10:58 +0000

    Added basic styling to header and nav

--- src/components/shared/layout/AppHeader.jsx
+++ src/components/shared/layout/AppHeader.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const HeaderStyle = {
+  display: 'flex',
+};
+
+const TitleStyle = {
+  margin: '0rem 1rem',
+  width: '100%',
+};
+
+const NavStyle = {
+  display: 'flex',
+  alignItems: 'center',
+};
+
+const LinkStyle = {
+  padding: '0rem 1rem',
+  border: '1px red solid',
+};
+
 const AppHeader = () => (
-  <header>
-    <h1>Nick Smirnoff</h1>
-    <Link to="/">Home</Link>
-    <br />
-    <Link to="/about">About</Link>
-    <br />
-    <Link to="/projects">Projects</Link>
+  <header style={HeaderStyle}>
+    <div style={TitleStyle}>
+      <h1>Nick Smirnoff</h1>
+    </div>
+    <nav style={NavStyle}>
+      <Link style={LinkStyle} to='/'>
+        Home
+      </Link>
+      <Link style={LinkStyle} to='/about'>
+        About
+      </Link>
+      <Link style={LinkStyle} to='/projects'>
+        Projects
+      </Link>
+    </nav>
   </header>
 );
 
