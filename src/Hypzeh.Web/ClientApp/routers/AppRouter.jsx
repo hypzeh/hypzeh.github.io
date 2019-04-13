ad9439c3fde601371ee4566913bbdd7491b76887
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-13 19:07:59 +0100

    Added AppHeader to App

--- src/Hypzeh.Web/ClientApp/routers/AppRouter.jsx
+++ src/Hypzeh.Web/ClientApp/routers/AppRouter.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import AppHeader from '../layout/AppHeader';
+
 const AppRouter = () => (
   <BrowserRouter>
+    <AppHeader />
     <Switch>
       <Route exact path='/' render={() => <h1>Home.</h1>} />
     </Switch>
