9b1fe72d00ded84adb0d1ba1944e180d4d8f38cc
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-21 21:50:00 +0100

    Updated AppRouter to use AppMain for main content

--- src/Hypzeh.Web/ClientApp/components/routers/AppRouter.jsx
+++ src/Hypzeh.Web/ClientApp/components/routers/AppRouter.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import AppHeader from '../layout/AppHeader';
+import AppMain from '../layout/AppMain';
 
 const AppRouter = () => (
   <BrowserRouter>
     <AppHeader />
-    <Switch>
-      <Route exact path='/' render={() => <h1>Home.</h1>} />
-    </Switch>
+    <AppMain>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Home.</h1>} />
+      </Switch>
+    </AppMain>
   </BrowserRouter>
 );
 
