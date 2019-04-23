0ff108f0a5a63a92be267586f4113fc186e4a8c6
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-23 22:14:31 +0100

    Updated App to use React Hooks with a Loader

--- src/Hypzeh.Web/ClientApp/App.jsx
+++ src/Hypzeh.Web/ClientApp/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import Loader from './components/shared/Loader';
 import AppRouter from './components/routers/AppRouter';
-
 import './styles/sass/app.scss';
 
-const App = () => (
-  <div className="App">
-    <AppRouter />
-  </div>
-);
+const App = () => {
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => setTimeout(() => setShowLoader(false), 1000), []);
+
+  return (
+    <div className="App">
+      {
+        showLoader
+          ? <Loader />
+          : <AppRouter />
+      }
+    </div>
+  );
+};
 
 export default App;
