1d01d8137cdc5d40d9b453e8e22380349dd3c19d
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-23 20:44:52 +0100

    Added routes

--- src/Hypzeh.Web/ClientApp/components/routers/AppRouter.jsx
+++ src/Hypzeh.Web/ClientApp/components/routers/AppRouter.jsx
@@ -10,6 +10,10 @@ const AppRouter = () => (
     <AppMain>
       <Switch>
         <Route exact path='/' render={() => <h1>Home.</h1>} />
+        <Route exact path='/about' render={() => <h1>About.</h1>} />
+        <Route exact path='/projects' render={() => <h1>Projects.</h1>} />
+        <Route exact path='/contact' render={() => <h1>Contact.</h1>} />
+        <Route path="*" render={() => <h1>NOT FOUND.</h1>} />
       </Switch>
     </AppMain>
   </BrowserRouter>
