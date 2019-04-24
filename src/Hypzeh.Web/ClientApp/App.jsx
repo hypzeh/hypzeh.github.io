6871831b96a7203108630e628beef593182f72d6
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-24 23:04:21 +0100

    Removed simulated App Loader

--- src/Hypzeh.Web/ClientApp/App.jsx
+++ src/Hypzeh.Web/ClientApp/App.jsx
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import Loader from './components/shared/Loader';
 import AppRouter from './components/routers/AppRouter';
 import './styles/sass/app.scss';
 
-const App = () => {
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => setTimeout(() => setShowLoader(false), 1000), []);
-
-  return (
-    <div className="App">
-      {
-        showLoader
-          ? <Loader />
-          : <AppRouter />
-      }
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <AppRouter />
+  </div>
+);
 
 export default App;
