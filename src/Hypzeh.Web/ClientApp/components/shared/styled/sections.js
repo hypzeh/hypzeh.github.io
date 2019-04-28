ee9aacd784cfed1b78966900a5c9dac1d366b640
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 20:14:18 +0100

    Added styled sections

---
+++ src/Hypzeh.Web/ClientApp/components/shared/styled/sections.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const PrimarySection = styled.section`
+  background: #212121;
+`;
+
+export const SecondarySection = styled.section`
+  background: #383838;
+`;
