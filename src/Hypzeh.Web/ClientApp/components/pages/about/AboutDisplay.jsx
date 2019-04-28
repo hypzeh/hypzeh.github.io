092bbb0007265150f9bad812d3b2f2e0aa3b36f3
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 20:14:52 +0100

    Created AboutDisplay component

---
+++ src/Hypzeh.Web/ClientApp/components/pages/about/AboutDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { PrimarySection } from '../../shared/styled/sections';
+
+const AboutDisplay = () => (
+  <PrimarySection>
+    <span>About.</span>
+  </PrimarySection>
+);
+
+export default AboutDisplay;
