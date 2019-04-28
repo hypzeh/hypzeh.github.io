3c672f3a92b036eb3804c5e7aaf1009809f47787
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 21:44:24 +0100

    Added basic style to AppFooter

--- src/Hypzeh.Web/ClientApp/components/layout/AppFooter.jsx
+++ src/Hypzeh.Web/ClientApp/components/layout/AppFooter.jsx
@@ -3,16 +3,48 @@ import styled from 'styled-components';
 
 const AppFooter = () => (
   <Footer>
-    <span>TEST</span>
+    <Top>Top</Top>
+    <Middle>Middle</Middle>
+    <Bottom>
+      <div>&copy; Nick Smirnoff</div>
+      <div>NS</div>
+      <div>
+        <a href="/">LI</a>
+        <a href="/">TW</a>
+        <a href="/">GH</a>
+      </div>
+    </Bottom>
   </Footer>
 );
 
 const Footer = styled.footer`
   background: black;
-  height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 20rem;
   position: sticky;
   bottom: 0;
   z-index: -1;
+`;
+
+const Top = styled.div`
+  border: 1px green solid;
+`;
+
+const Middle = styled.div`
+  border: 1px green solid;
+`;
+
+const Bottom = styled.div`
+  border: 1px green solid;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  a {
+    margin: 0 .5rem;
+  }
 `;
 
 export default AppFooter;
