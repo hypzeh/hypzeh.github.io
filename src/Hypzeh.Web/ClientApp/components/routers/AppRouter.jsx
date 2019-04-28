ad59dcccc91acba98aee1af29c8a7f8804c74168
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 20:16:03 +0100

    Updated AppRouter to render appropriate page components

--- src/Hypzeh.Web/ClientApp/components/routers/AppRouter.jsx
+++ src/Hypzeh.Web/ClientApp/components/routers/AppRouter.jsx
@@ -4,16 +4,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AppHeader from '../layout/AppHeader';
 import AppMain from '../layout/AppMain';
 import AppFooter from '../layout/AppFooter';
+import { HomeDisplay, AboutDisplay, ProjectsDisplay } from '../pages';
 
 const AppRouter = () => (
   <BrowserRouter>
     <AppHeader />
     <AppMain>
       <Switch>
-        <Route exact path="/" render={() => <h1>Home.</h1>} />
-        <Route exact path="/about" render={() => <h1>About.</h1>} />
-        <Route exact path="/projects" render={() => <h1>Projects.</h1>} />
-        <Route exact path="/contact" render={() => <h1>Contact.</h1>} />
+        <Route exact path="/" component={HomeDisplay} />
+        <Route exact path="/about" component={AboutDisplay} />
+        <Route exact path="/projects" component={ProjectsDisplay} />
         <Route path="*" render={() => <h1>NOT FOUND.</h1>} />
       </Switch>
     </AppMain>
