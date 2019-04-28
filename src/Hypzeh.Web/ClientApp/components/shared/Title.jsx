613aa9513a073b97729cd8f6fd953dec2d49f469
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 20:14:32 +0100

    Created Title component

---
+++ src/Hypzeh.Web/ClientApp/components/shared/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = () => (
+  <TitleContainer>
+    <h1>Home.</h1>
+  </TitleContainer>
+);
+
+const TitleContainer = styled.div`
+  width: fit-content;
+`;
+
+export default Title;
