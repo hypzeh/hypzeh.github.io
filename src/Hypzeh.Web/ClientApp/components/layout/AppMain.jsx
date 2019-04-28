b9362633ef44bd2c4b4bccfc8fa532550f61f9dd
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 20:11:50 +0100

    Added style to AppMain

--- src/Hypzeh.Web/ClientApp/components/layout/AppMain.jsx
+++ src/Hypzeh.Web/ClientApp/components/layout/AppMain.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { propTypes, defaultProps } from '../../types/layout/appMain';
 
 const AppMain = ({ children }) => (
-  <main>
+  <Main>
     {children}
-  </main>
+  </Main>
 );
+
+const Main = styled.main`
+  min-height: 100vh;
+  position: relative;
+  z-index: 1;
+  background: #212121;
+`;
 
 AppMain.propTypes = propTypes;
 AppMain.defaultProps = defaultProps;
