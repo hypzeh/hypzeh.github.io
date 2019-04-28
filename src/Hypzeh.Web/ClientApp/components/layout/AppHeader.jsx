2da09c4362a2bafa42a89c03198f9ffbabc3d643
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 22:55:08 +0100

    Added a temporary error nav link for testing

--- src/Hypzeh.Web/ClientApp/components/layout/AppHeader.jsx
+++ src/Hypzeh.Web/ClientApp/components/layout/AppHeader.jsx
@@ -7,6 +7,7 @@ const AppHeader = () => (
     <Navbar>
       <NavBrand to="/">Nick Smirnoff</NavBrand>
       <NavItems>
+        <NavItem to="/error">☠</NavItem>
         <NavItem to="/about">ABOUT</NavItem>
         <NavItem to="/projects">PROJECTS</NavItem>
         <a href="mailto:test@test.test">CONTACT</a>
