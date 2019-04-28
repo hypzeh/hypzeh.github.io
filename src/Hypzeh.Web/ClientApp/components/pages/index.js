a8823e1c7c123123b47f80789b24caf58ae8b308
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 22:54:31 +0100

    Added Error component to page namespace

--- src/Hypzeh.Web/ClientApp/components/pages/index.js
+++ src/Hypzeh.Web/ClientApp/components/pages/index.js
@@ -1,9 +1,11 @@
 import HomeDisplay from './home/HomeDisplay';
 import AboutDisplay from './about/AboutDisplay';
 import ProjectsDisplay from './projects/ProjectsDisplay';
+import Error from './errors/Error';
 
 export {
   HomeDisplay,
   AboutDisplay,
   ProjectsDisplay,
+  Error,
 };
