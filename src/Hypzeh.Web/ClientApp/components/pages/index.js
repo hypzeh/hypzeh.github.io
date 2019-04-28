17c402e24e919874d3f76fd4383eedde1d9686cf
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 20:15:42 +0100

    Created pages namespace export

---
+++ src/Hypzeh.Web/ClientApp/components/pages/index.js
@@ -0,0 +1,9 @@
+import HomeDisplay from './home/HomeDisplay';
+import AboutDisplay from './about/AboutDisplay';
+import ProjectsDisplay from './projects/ProjectsDisplay';
+
+export {
+  HomeDisplay,
+  AboutDisplay,
+  ProjectsDisplay,
+};
