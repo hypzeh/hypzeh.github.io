ff598f439a1759b6c8cfef4306b57f62c9ca92a9
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-28 20:15:22 +0100

    Created HomeDisplay component

---
+++ src/Hypzeh.Web/ClientApp/components/pages/home/HomeDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { PrimarySection } from '../../shared/styled/sections';
+import Title from '../../shared/Title';
+
+const HomeDisplay = () => (
+  <PrimarySection>
+    <Title />
+  </PrimarySection>
+);
+
+export default HomeDisplay;
