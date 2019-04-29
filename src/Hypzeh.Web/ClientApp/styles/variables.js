08f0fedd6655a0c051c3061d83f6d0ea31368a22
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-29 23:17:57 +0100

    Created style variables

---
+++ src/Hypzeh.Web/ClientApp/styles/variables.js
@@ -0,0 +1,8 @@
+export const appHeaderHeight = '3rem';
+export const appFooterHeight = '20rem';
+
+export const grey = '#212121';
+export const white = '#fff';
+
+export const primaryBackground = grey;
+export const primaryColour = white;
