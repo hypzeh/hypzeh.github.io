add5d267c8ed02ff8cb56811cf0b1dc423edc4ff
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-04-29 23:18:42 +0100

    Added ContactDisplay to namespace

--- src/Hypzeh.Web/ClientApp/components/pages/index.js
+++ src/Hypzeh.Web/ClientApp/components/pages/index.js
@@ -1,11 +1,13 @@
 import HomeDisplay from './home/HomeDisplay';
 import AboutDisplay from './about/AboutDisplay';
 import ProjectsDisplay from './projects/ProjectsDisplay';
+import ContactDisplay from './contact/ContactDisplay';
 import Error from './errors/Error';
 
 export {
   HomeDisplay,
   AboutDisplay,
   ProjectsDisplay,
+  ContactDisplay,
   Error,
 };
