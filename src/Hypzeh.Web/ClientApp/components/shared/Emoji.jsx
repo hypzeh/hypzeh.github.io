bae124054441fac4801bfb8c49efa4fbb1058ec5
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-05-05 22:36:48 +0100

    Created Emoji component

---
+++ src/Hypzeh.Web/ClientApp/components/shared/Emoji.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { randomEmoji } from '../../lib/emojis';
+
+const Emoji = ({ emoji, label, enableRoulette }) => {
+  const [state, setState] = useState({ emoji, label });
+
+  const onMouseEnter = () => {
+    if (!enableRoulette) return;
+
+    setState(randomEmoji());
+  };
+
+  return (
+    <Span role="img" aria-label={state.label || ''} aria-hidden={state.label ? 'false' : 'true'} onMouseEnter={onMouseEnter}>
+      {state.emoji}
+    </Span>
+  );
+};
+
+const Span = styled.span`
+  cursor: default;
+`;
+
+Emoji.propTypes = {
+  emoji: PropTypes.string,
+  label: PropTypes.string,
+  enableRoulette: PropTypes.bool,
+};
+Emoji.defaultProps = {
+  ...randomEmoji(),
+  enableRoulette: false,
+};
+
+export default Emoji;
