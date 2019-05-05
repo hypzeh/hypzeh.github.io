8f7b15cf332136fba3163ff7e8daf09948bc1138
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-05-05 23:17:17 +0100

    Updated format of emoji lib

--- src/Hypzeh.Web/ClientApp/lib/emojis.js
+++ src/Hypzeh.Web/ClientApp/lib/emojis.js
@@ -1,40 +1,45 @@
+const insert = (emoji, label) => ({ emoji, label });
+
 export const emojis = [
-  {
-    emoji: '🐧',
-    label: 'Penguin',
-  },
-  {
-    emoji: '🐤',
-    label: 'Baby Chick',
-  },
-  {
-    emoji: '🌭',
-    label: 'Hot Dog',
-  },
-  {
-    emoji: '💩',
-    label: 'Pile of Poo',
-  },
-  {
-    emoji: '🔥',
-    label: 'Fire',
-  },
-  {
-    emoji: '❤️',
-    label: 'Red Heart',
-  },
-  {
-    emoji: '😂',
-    label: 'Face With Tears of Joy',
-  },
-  {
-    emoji: '🤔',
-    label: 'Thinking Face',
-  },
-  {
-    emoji: '👍',
-    label: 'Thumbs Up',
-  },
+  // A
+  // B
+  // C
+  insert('🐤', 'Baby Chick'),
+  // D
+  // E
+  // F
+  insert('😂', 'Face With Tears of Joy'),
+  insert('🔥', 'Fire'),
+  insert('🙏', 'Folded Hands'),
+  // G
+  insert('🌍', 'Globe Showing Europe-Africa'),
+  insert('🎓', 'Graduation Cap'),
+  // H
+  insert('🌭', 'Hot Dog'),
+  // I
+  // J
+  // K
+  // L
+  // M
+  // N
+  // O
+  // P
+  insert('🐧', 'Penguin'),
+  insert('💩', 'Pile of Poo'),
+  // Q
+  // R
+  insert('❤️', 'Red Heart'),
+  // S
+  insert('😊', 'Smiling Face With Smiling Eyes'),
+  // T
+  insert('🤔', 'Thinking Face'),
+  insert('👍', 'Thumbs Up'),
+  // U
+  // V
+  // W
+  // X
+  // Y
+  // Z
 ];
 
 export const randomEmoji = () => (emojis[Math.floor(Math.random() * emojis.length)]);
