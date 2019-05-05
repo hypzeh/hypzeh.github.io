16d309a47af5f4a11b8ff52ac79923509928ff80
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-05-05 23:05:17 +0100

    Added emojis

--- src/Hypzeh.Web/ClientApp/lib/emojis.js
+++ src/Hypzeh.Web/ClientApp/lib/emojis.js
@@ -11,6 +11,30 @@ export const emojis = [
     emoji: '🌭',
     label: 'Hot Dog',
   },
+  {
+    emoji: '💩',
+    label: 'Pile of Poo',
+  },
+  {
+    emoji: '🔥',
+    label: 'Fire',
+  },
+  {
+    emoji: '❤️',
+    label: 'Red Heart',
+  },
+  {
+    emoji: '😂',
+    label: 'Face With Tears of Joy',
+  },
+  {
+    emoji: '🤔',
+    label: 'Thinking Face',
+  },
+  {
+    emoji: '👍',
+    label: 'Thumbs Up',
+  },
 ];
 
 export const randomEmoji = () => (emojis[Math.floor(Math.random() * emojis.length)]);
