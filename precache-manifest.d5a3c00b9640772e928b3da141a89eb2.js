43dc1549021f0f491a1525f6c3c22573213f8cf8
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-05-11 20:23:15 +0100

    Update to React and new domain
    Under construction

---
+++ precache-manifest.d5a3c00b9640772e928b3da141a89eb2.js
@@ -0,0 +1,64 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af291eaff516cc9dde17a552b79fd7e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6b05e2143f1dd16dbad",
+    "url": "1.css"
+  },
+  {
+    "url": "error.383e1ab0a79d2c83641a.chunk.js"
+  },
+  {
+    "revision": "e58640e625a1666779bbbb88cc5197ca",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "fonts/open-sans-v16-latin-regular.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "fonts/open-sans-v16-latin-regular.woff2"
+  },
+  {
+    "url": "home.dbc200dd457881f2b77a.chunk.js"
+  },
+  {
+    "url": "icon_144x144.ee16b564b390fcfc8c480d9fa4cea45f.png"
+  },
+  {
+    "url": "icon_192x192.087613d60f5b3e20d25c42cd62ecbd3a.png"
+  },
+  {
+    "url": "icon_36x36.ad476c330665e72b98feccbd7f68e434.png"
+  },
+  {
+    "url": "icon_48x48.41e9cb0571e33b57810facbc7156e648.png"
+  },
+  {
+    "url": "icon_512x512.18da2f928266a319c1bff67e2076ab73.png"
+  },
+  {
+    "url": "icon_72x72.e60f810084c1fa663654c330c3170422.png"
+  },
+  {
+    "url": "icon_96x96.680f24842092572f779add90e4921d87.png"
+  },
+  {
+    "url": "main.1333500986f22af2b593.chunk.js"
+  },
+  {
+    "url": "main~error~home.f2fabde42ef8ccf81036.chunk.js"
+  },
+  {
+    "url": "manifest.2cc7596e088be026d3ed226cf905bcef.json"
+  },
+  {
+    "url": "runtime~main.9b8943530d5cc6787b19.js"
+  },
+  {
+    "url": "vendor.b6b05e2143f1dd16dbad.chunk.js"
+  }
+]);
