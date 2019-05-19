323a08d73418a581b3177b0e6b8a1cb50e82566b
Nick Smirnoff ☕ <nick.smirnoff@live.co.uk>
2019-05-19 23:25:30 +0100

    Added to work experience

--- src/Hypzeh.Web/ClientApp/components/pages/About/components/WorkExperience/experience.js
+++ src/Hypzeh.Web/ClientApp/components/pages/About/components/WorkExperience/experience.js
@@ -2,12 +2,24 @@ import moment from 'moment';
 
 const experience = [
   {
+    company: 'Zupa',
+    positions: [
+      {
+        title: 'Software Developer',
+        location: 'Southampton, United Kingdom',
+        description: 'this is a description about my time as a software developer...',
+        start: moment('2018-10').startOf('month'),
+        end: moment().endOf('month'),
+      },
+    ],
+  },
+  {
     company: 'APD Communications',
     positions: [
       {
         title: 'Software Engineer',
         location: 'Hull, United Kingdom',
-        description: 'this is a description about my time as a graduate software engineer...',
+        description: 'this is a description about my time as a software engineer...',
         start: moment('2018-05').startOf('month'),
         end: moment('2018-08').endOf('month'),
       },
@@ -17,6 +29,18 @@ const experience = [
         description: 'this is a description about my time as a graduate software engineer...',
         start: moment('2017-08').startOf('month'),
         end: moment('2018-05').endOf('month'),
+      },
+    ],
+  },
+  {
+    company: 'Seed Software',
+    positions: [
+      {
+        title: 'Software Engineer (Part-Time)',
+        location: 'Hull, United Kingdom',
+        description: 'this is a description about my time as a part-time software engineer...',
+        start: moment('2016-09').startOf('month'),
+        end: moment('2017-05').endOf('month'),
       },
     ],
   },
