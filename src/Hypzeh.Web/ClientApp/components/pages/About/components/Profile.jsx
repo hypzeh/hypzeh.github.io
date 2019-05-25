e9aaa5348399aebf9d3b265516159dec93bc770e
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-05-25 22:19:00 +0100

    Updated profile responsive style

--- src/Hypzeh.Web/ClientApp/components/pages/About/components/Profile.jsx
+++ src/Hypzeh.Web/ClientApp/components/pages/About/components/Profile.jsx
@@ -6,23 +6,41 @@ import profile from '../../../../assets/img/profile.jpg';
 
 const Conatiner = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
+
+  ${media.medium`
+    flex-direction: column;
+    align-items: center;
+  `}
 `;
 
 const Image = styled.div`
-  width: 10rem;
-  height: 10rem;
-  margin-top: 1rem;
-  margin-right: 1rem;
+  width: 30rem;
+  height: 30rem;
+  margin: 1rem 1rem 1rem 0;
   background-color: white;
   border-radius: 50%;
+
+  ${media.large`
+    width: 40vw;
+    height: 40vw;
+  `}
+
+  ${media.medium`
+    margin: 1rem 1rem 0 1rem;
+  `}
+
+  ${media.small`
+    width: 10rem;
+    height: 10rem;
+  `}
 
   img {
     width: inherit;
     object-fit: cover;
     border-radius: 50%;
-    padding: 2px;
+    padding: .25rem;
   }
 `;
 
@@ -39,6 +57,8 @@ const Profile = () => (
     <Information>
       <h2>About</h2>
       <p>{'I\'m Nick Smirnoff - a developer from the United Kingdom!'}</p>
+      <p>{'I\'ve performed suspicion in certainty so frankness by attention pretended. Newspaper or in tolerably education enjoyment.'}</p>
+      <p>{'I\'ve Elderly up chicken do at feeling is. Like seen drew no make fond at on rent. Behaviour extremely her explained situation yet september gentleman are who.'}</p>
     </Information>
   </Conatiner>
 );
