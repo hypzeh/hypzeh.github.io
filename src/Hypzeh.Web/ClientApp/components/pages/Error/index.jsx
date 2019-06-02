e9547ede502addd31a915973e79b5e33be0fcc4e
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-06-02 20:14:34 +0100

    Moved Error content to Message component

--- src/Hypzeh.Web/ClientApp/components/pages/Error/index.jsx
+++ src/Hypzeh.Web/ClientApp/components/pages/Error/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import Page, { Header, Section } from '../../shared/Page';
 import Title from '../../shared/Title';
+import Message from './components/Message';
 
 const propTypes = {
-  history: PropTypes.shape().isRequired,
   code: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
@@ -21,32 +20,16 @@ const defaultProps = {
   message: 'Something went wrong!',
 };
 
-const Error = ({
-  history,
-  code,
-  title,
-  message,
-}) => {
-  console.log(history);
-  console.log(code);
-  console.log(message);
-
-  return (
-    <Page name={title}>
-      <Header>
-        <Title title={code} />
-      </Header>
-      <Section>
-        <p>{message}</p>
-        <p>
-          <button type="button" onClick={history.goBack}>Back</button>
-          |
-          <Link to="/">Home</Link>
-        </p>
-      </Section>
-    </Page>
-  );
-};
+const Error = ({ code, title, message }) => (
+  <Page name={title}>
+    <Header>
+      <Title title={code} />
+    </Header>
+    <Section>
+      <Message message={message} />
+    </Section>
+  </Page>
+);
 
 Error.propTypes = propTypes;
 Error.defaultProps = defaultProps;
