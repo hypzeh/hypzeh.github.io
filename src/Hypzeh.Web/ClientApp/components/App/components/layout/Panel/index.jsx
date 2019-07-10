fba72e42cd28c87bf3aa69e3c4f1a47b8f88a005
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-07-10 22:47:51 +0100

    Added media query to panel

--- src/Hypzeh.Web/ClientApp/components/App/components/layout/Panel/index.jsx
+++ src/Hypzeh.Web/ClientApp/components/App/components/layout/Panel/index.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import media from '../../../utils/style/media';
 import { Scroller } from '../../shared';
 
 const Container = styled.div`
   border: 5px solid green;
-  width: 15rem;
   flex-shrink: 0;
   position: relative;
   display: flex;
   flex-direction: column;
   background-color: #2f3136;
   overflow: hidden;
+  z-index: 1;
+
+  ${media.medium`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+  `}
 `;
 
 const Panel = () => (
   <Container>
-    <Scroller>
+    <Scroller width="15rem">
       <h1>1</h1>
       <h1>1</h1>
       <h1>1</h1>
