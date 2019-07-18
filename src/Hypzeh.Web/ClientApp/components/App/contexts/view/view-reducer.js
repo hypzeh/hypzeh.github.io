604b50cb4c410a6fe3c789ce600cbe392771ca9a
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-07-18 22:55:29 +0100

    Updated formatting

--- src/Hypzeh.Web/ClientApp/components/App/contexts/view/view-reducer.js
+++ src/Hypzeh.Web/ClientApp/components/App/contexts/view/view-reducer.js
@@ -4,8 +4,10 @@ const reducer = (state, action) => {
   switch (action.type) {
   case PANEL_OPEN:
     return { ...state, isPanelOpen: true };
+
   case PANEL_CLOSE:
     return { ...state, isPanelOpen: false };
+
   default:
     return state;
   }
