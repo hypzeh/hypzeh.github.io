cdf34a26b93f926483fc0cea332e4c1fabf9a0ca
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-07-18 22:54:53 +0100

    Created navigation lib

---
+++ src/Hypzeh.Web/ClientApp/components/App/utils/navigation/sections/ns.js
@@ -0,0 +1,26 @@
+const routing = {
+  internal: [
+    { path: '/', title: 'Home' },
+    { path: '/test', title: 'Test Page' },
+    { path: '/test', title: 'Test Page1' },
+    { path: '/test', title: 'Test Page2' },
+    { path: '/test', title: 'Test Page3' },
+    { path: '/test', title: 'Test Page4' },
+    { path: '/test', title: 'Test Page5' },
+    { path: '/test', title: 'Test Page6' },
+    { path: '/test', title: 'Test Page7' },
+    { path: '/test', title: 'Test Page8' },
+    { path: '/test', title: 'Test Page9' },
+    { path: '/test', title: 'Test Page10' },
+    { path: '/test', title: 'Test Page11' },
+    { path: '/test', title: 'Test Page12' },
+    { path: '/test', title: 'Test Page13' },
+    { path: '/test', title: 'Test Page14' },
+    { path: '/test', title: 'Test Page15' },
+  ],
+  external: [
+    { path: 'http://www.google.co.uk', title: 'Google' },
+  ],
+};
+
+export default routing;
