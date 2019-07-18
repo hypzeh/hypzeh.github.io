4760c490f699070d31e830d6e5da5cd77fc9822c
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-07-18 22:08:14 +0100

    Created internal and external link components

---
+++ src/Hypzeh.Web/ClientApp/components/App/components/layout/Panel/components/ExternalLink.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExternalLink = () => (
+  <div>
+    External Link
+  </div>
+);
+
+export default ExternalLink;
---
+++ src/Hypzeh.Web/ClientApp/components/App/components/layout/Panel/components/InternalLink.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InternalLink = () => (
+  <div>
+    Internal Link
+  </div>
+);
+
+export default InternalLink;
