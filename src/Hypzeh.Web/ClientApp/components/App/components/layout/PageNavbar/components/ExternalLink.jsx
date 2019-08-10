5c7e3855382eeec03932e1cb65c39e901ad0f1d5
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-08-10 19:18:14 +0100

    Added basic styling to external links

--- src/Hypzeh.Web/ClientApp/components/App/components/layout/PageNavbar/components/ExternalLink.jsx
+++ src/Hypzeh.Web/ClientApp/components/App/components/layout/PageNavbar/components/ExternalLink.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const propTypes = {
   name: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
 };
 
+const Link = styled.a`
+  padding: .5rem;
+  color: white;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #181818;
+  }
+
+  &.active {
+    background-color: #212121;
+  }
+`;
+
 const ExternalLink = ({ name, path }) => (
-  <a href={path}>{name}</a>
+  <Link href={path} aria-label={name} target="_blank" rel="noopener noreferrer">{name}</Link>
 );
 
 ExternalLink.propTypes = propTypes;
