4032e115e011031c6c0d4a340c9463a64e68b7d1
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2021-10-24 14:29:37 +0100

    Updated project version and dependencies

--- src/Hypzeh.Web/Program.cs
+++ src/Hypzeh.Web/Program.cs
@@ -1,20 +1,48 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
 
 namespace Hypzeh.Web
 {
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args)
-                .Build()
-                .Run();
-        }
+	public static class Program
+	{
+		public static int Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.MinimumLevel.Override("System", LogEventLevel.Warning)
+				.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+				.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+				.Enrich.FromLogContext()
+				.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+				.CreateLogger();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost
-                .CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+			try
+			{
+				Log.Information("Starting host...");
+				CreateHostBuilder(args).Build().Run();
+				return 0;
+			}
+			catch (Exception exception)
+			{
+				Log.Fatal(exception, "Hot terminated unexpectedly.");
+				return 1;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+					webBuilder.UseSerilog();
+				});
+	}
 }
